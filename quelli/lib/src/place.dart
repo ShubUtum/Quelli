099e361d73b2236a679c29be00c9e49e1e19724c
Patipon Riebpradit <patipon74@gmail.com>
2020-04-18 03:42:44 +0200

    [+] fix FAB, added confirmation dialog

--- quelli/lib/src/place.dart
+++ quelli/lib/src/place.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quelli/src/Timeslot.dart';
 import 'package:quelli/src/restaurantTitle.dart';
 
+enum ConfirmAction { ACCEPT, CANCEL }
 
 class Place extends StatefulWidget {
   @override
@@ -23,27 +24,55 @@ class PlaceState extends State<Place> {
         title: Text('Queue up'),
       ),
       body: Column(
-          children: <Widget>[
-            RestaurantTitle(),
-            TimeSlot()
-          ],
-        ),
+        children: <Widget>[
+          RestaurantTitle(),
+          TimeSlot()
+        ],
+      ),
 
       bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0,),
+
+        child: Container(height: 30.0,),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-
-        }),
-        tooltip: 'Queue up this place',
-
-        child: Icon(Icons.play_for_work,
-        size: 40,),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 4.0,
+        icon: const Icon(Icons.add),
+        label: const Text('Add your queue'),
+        onPressed: () async {
+          final ConfirmAction action = await _asyncConfirmDialog(context);
+          print("Confirm Action $action");
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
+
+  Future<ConfirmAction> _asyncConfirmDialog(BuildContext context) async {
+    return showDialog<ConfirmAction>(
+      context: context,
+      barrierDismissible: false, // user must tap button for close dialog!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Reset settings?'),
+          content: const Text(
+              'This will reset your device to its default factory settings.'),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop(ConfirmAction.CANCEL);
+              },
+            ),
+            FlatButton(
+              child: const Text('ACCEPT'),
+              onPressed: () {
+                Navigator.of(context).pop(ConfirmAction.ACCEPT);
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
 }
--- quelli/lib/src/restaurantTitle.dart
+++ quelli/lib/src/restaurantTitle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class RestaurantTitle extends StatelessWidget {
   const RestaurantTitle({Key key}) : super(key: key);
@@ -17,7 +18,11 @@ class RestaurantTitle extends StatelessWidget {
                 backgroundColor: Colors.brown.shade800,
                 child: Text('RE'),
             )),
-            const Text("What a good restaurant", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18))
+            const Text("What a good restaurant", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            SizedBox(height: 5),
+            const Text("Am Michaelianger 2, Oberschleissheim", style: TextStyle(color: Colors.grey, fontSize: 14)),
+            SizedBox(height: 5),
+            const Text("Closed - Open at 11:00-19:00", style: TextStyle(color:Colors.redAccent, fontSize: 12))
       ],
     ))
     ;
